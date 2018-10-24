86d33dd666d392027b10de136624b7c9b04c682a
Phthalaten <sbr@familienbruun.dk>
2018-10-24 13:19:45 -0700

    Did some codez and such fuck it hurts mah

--- ourCode/sketch_oct12a/sketch_oct12a.ino
+++ ourCode/sketch_oct12a/sketch_oct12a.ino
@@ -1,21 +1,42 @@
 //Reading stuff:
-int ForcePin = A1;
-int ForcePin2 = A2;
-int ForcePin3 = A3;
+int ForcePin = A0;
+int ForcePin2 = A1;
+int ForcePin3 = A2;
+int ForcePin4 = A3;
 int ForceReading = 0;
 int ForceReading2 = 0;
 int ForceReading3 = 0;
-int inPin = 2;
-int reading;
+int ForceReading4 = 0;
+
+/*--------------------------------------------------------------------*/
+
+//Button on/off:
+const int buttonPin = 3;
+const int ledPin = 2;
+
+int ledState = LOW;
+int buttonState;
+int lastButtonState = HIGH;
+
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 500;
+
+/*--------------------------------------------------------------------*/
 
 //SD-card stuff:
 //#include <SPI.h>
 //#include <SD.h>
 
+/*--------------------------------------------------------------------*/
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);
+
+  //Button stuff:
+  pinMode(buttonPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, ledState);
 
   //SD-card stuff:
   //SD.begin(4);
@@ -24,30 +45,59 @@ void setup() {
 }
 
 void loop() {
-  reading = digitalRead(inPin);
-  Serial.println(reading);
-  delay(200);
-  if (reading == HIGH) {
+
+  //Button stuff:
+  //read the button pin, if pressed will be high, if not pressed will be low
+  int reading = digitalRead(buttonPin);
+  //in the case that the reading is not equal to the last state set the last debounce time to current millis time
+  if (reading != lastButtonState)  {
+    lastDebounceTime = millis();
+  }
+  //check the difference between current time and last registered button press time, if it's greater than user defined delay then change the LED state as it's not a bounce
+  if ((millis()-lastDebounceTime) > debounceDelay) {
+    if (reading != buttonState) {
+      buttonState = reading;
+    if (buttonState == HIGH) {
+      ledState = !ledState;
+      }
+    }
+  }
+  //set the LED
+  digitalWrite(ledPin, ledState);
+
+  //save the reading. next time through the loop the state of the reading will be known as the lastButtonState
+  lastButtonState = reading;
+
+/*--------------------------------------------------------------------*/
+
+  //Reading-stuff:
+  if (ledState == HIGH) {
     ForceReading = analogRead(ForcePin);
-    Serial.print("Small sensor:");
+    delay(10);
+    ForceReading = analogRead(ForcePin);
+    Serial.print("Sensor 1: ");
     Serial.println(ForceReading);
     //dataFile.print(ForceReading);
     //dataFile.print(" ~ ");
-    delay(200);
     ForceReading2 = analogRead(ForcePin2);
-    Serial.print("Big sensor:");
+    delay(10);
+    ForceReading2 = analogRead(ForcePin2);
+    Serial.print("Sensor 2: ");
     Serial.println(ForceReading2);
     //dataFile.println(ForceReading2);
-    delay(200);
     ForceReading3 = analogRead(ForcePin3);
-    Serial.print("Jack sensor:");
+    delay(10);
+    ForceReading3 = analogRead(ForcePin3);
+    Serial.print("Sensor 3: ");
     Serial.println(ForceReading3);
-    delay(200);
-    digitalWrite(LED_BUILTIN, HIGH);
-  } else {
-    digitalWrite(LED_BUILTIN, LOW);
+    ForceReading4 = analogRead(ForcePin4);
+    delay(10);
+    ForceReading4 = analogRead(ForcePin4);
+    Serial.print("Sensor 4: ");
+    Serial.println(ForceReading4);
+    delay(1500);
+  }
 
     // ADD STUFF FOR SD-CARD closure https://randomnerdtutorials.com/guide-to-sd-card-module-with-arduino/
     
-  }
 }
