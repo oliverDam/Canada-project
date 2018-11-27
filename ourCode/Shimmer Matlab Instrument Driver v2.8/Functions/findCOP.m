9b4e9f5a33e020d2e851686985f334187dcb9b3c
Phthalaten <sbr@familienbruun.dk>
2018-11-27 15:56:26 -0800

    Changes in COP

--- ourCode/Shimmer Matlab Instrument Driver v2.8/Functions/findCOP.m
+++ ourCode/Shimmer Matlab Instrument Driver v2.8/Functions/findCOP.m
@@ -1,16 +1,9 @@
-function [COP] = findCOP(movement, pressure)
+function [COP,COPR,COPL] = findCOP(movement, p)
 
 %%Go for log in this stuff!
 %%Make something that says "if movement is yes and pressure on one foot is
 %%higher than other foot, then we only stand on one foot otherwise we are
-%%on two footses"
-
-%Normalized pressure on each foot based on initial standstill:
-%meanPress = mean(pressure(200:451));
-%normPress = pressure-meanPress;
-%p = log(pressure);
-%p = normalizeData(pressure,'max');
-p=pressure;
+%%on two footses" or maybe not
 
 %Find COP hopefully
 for i = 1:length(movement)
@@ -20,16 +13,13 @@ for i = 1:length(movement)
         p(i,k) = p(i,k)/sum(p(i,:));
     end
     
+    %COP for both feet:
     COP(i,1) = sum(p(i,1:3))-sum(p(i,4:6));
+    COP(i,2) = sum(p(i,[1 4]))-sum(p(i,[2:3 5:6]));
     
-    %If we don't have movement the Y-direction depends on both feet:
-    %if movement == 0
-        COP(i,2) = sum(p(i,[1 4]))-sum(p(i,[2:3 5:6]));
-    %else %Else it is calculated based on the y-load on a single foot:
-    %    if sum(p(i,1:2)) >= sum(p(i,3:4))
-    %        COP(i,2) = (p(i,1)-p(i,2))/sum(p(i,1:2));
-    %    else
-    %        COP(i,2) = p(i,3)-p(i,4)/sum(p(i,3:4));
-    %    end
-    %end
+    %Left and right COP:
+    COPR(i,1) = sum(p(i,1))-sum(p(i,2));
+    COPR(i,2) = sum(p(i,[1:2]))-sum(p(i,3));
+    COPL(i,1) = sum(p(i,4))-sum(p(i,5));
+    COPL(i,2) = sum(p(i,[4:5]))-sum(p(i,6));
 end
