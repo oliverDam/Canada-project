728e093d42a5eaada28d6d07e8c4b1b4d3c5a9a6
oliverDam <odamsg14@student.aau.dk>
2018-11-27 17:29:19 -0800

    changes to test, instrumentation, dataAcquisition sections. also plot of FSR test

--- ourCode/Shimmer Matlab Instrument Driver v2.8/Functions/FSRTestPlot.m
+++ ourCode/Shimmer Matlab Instrument Driver v2.8/Functions/FSRTestPlot.m
@@ -15,6 +15,6 @@ hold on;
 plot(FTD(:,1),FTD(:,7))
 l = legend('FSR1','FSR2','FSR3','FSR4','FSR5','FSR6');
 l.FontSize = 18;
-t = title('Test of FSR with 1KG applied force'); %or other weight
+t = title('Test of FSR with 1kg applied force'); %or other weight
 t.FontSize = 20;
 set(gca,'FontSize',18)
