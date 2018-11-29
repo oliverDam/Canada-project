a5d77c0f64a01f18b0f2628b4f3d3f9154f9247f
oliverDam <odamsg14@student.aau.dk>
2018-11-28 17:00:40 -0800

    smarter script function with help describtion

--- ourCode/Shimmer Matlab Instrument Driver v2.8/Functions/FSRTestPlot.m
+++ ourCode/Shimmer Matlab Instrument Driver v2.8/Functions/FSRTestPlot.m
@@ -1,7 +1,17 @@
-%load the FSRTestDATA
-function result = FSRTestPlot(data)
-FTD = table2array(data); %loading data directly from the .TXT file will make data a table
-figure(1)
+%%Function to plot test data collected from the Force Sensitive Resistors
+%(FSR). Starts by converting input data from table to matrix. If input data
+%is on table form (loaded directly from .txt-file) inputIsTable should be 1. 
+%If input data is on matrix form, inputIsTable should be 0.
+%
+% [output] = FSRTestPlot(data, inputIsTable);
+
+function result = FSRTestPlot(data, inputIsTable)
+    if inputIsTable == 1
+        FTD = table2array(data);
+    else
+        FTD = data;
+    end
+figure;
 plot(FTD(:,1),FTD(:,2))
 hold on;
 plot(FTD(:,1),FTD(:,3))
@@ -18,3 +28,4 @@ l.FontSize = 18;
 t = title('Test of FSR with 1kg applied force'); %or other weight
 t.FontSize = 20;
 set(gca,'FontSize',18)
+end
