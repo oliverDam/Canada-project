4e688209ca95f99ba2f9afd9dc65840ca87098eb
oliverDam <odamsg14@student.aau.dk>
2018-12-03 14:34:37 -0800

    gyro fft plot function update på labels og fontsize

--- ourCode/Shimmer Matlab Instrument Driver v2.8/Functions/doGyroFFT.m
+++ ourCode/Shimmer Matlab Instrument Driver v2.8/Functions/doGyroFFT.m
@@ -1,5 +1,5 @@
 %fft of gyroscopes
-function [result] = doGyroFFT(data,plot)
+function [result] = doGyroFFT(data,doPlot)
 
 % data(1) = data1;
 % data(2) = data2;
@@ -22,24 +22,36 @@ end
     xlimits = [0 15];
     ylimits = [0 3000];
 
-if plot==1
+if doPlot==1
     figure;
     subplot(S,1,1)
     plot(f,P1(:,1),'b')
     legend('X')
+    set(gca,'FontSize',14);
     xlim(xlimits)
     ylim(ylimits)
     subplot(S,1,2)
     plot(f,P1(:,2),'r')
     legend('Y')
+    set(gca,'FontSize',14);
     xlim(xlimits)
     ylim(ylimits)
     try
         subplot(S,1,3)
         plot(f,P1(:,3),'g')
         legend('Z')
+        set(gca,'FontSize',14);
         xlim(xlimits)
         ylim(ylimits)
+        
+        a = axes;
+        t1 = ylabel('Power');
+        t2 = xlabel('Frequency');
+        t1.FontSize = 20;
+        a.Visible = 'off'; % set(a,'Visible','off');
+        t1.Visible = 'on'; % set(t1,'Visible','on');
+        t2.Visible = 'on';
+        set(gca,'FontSize',20);
     catch
         lol = 420;
     end
